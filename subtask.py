c38525ab1615f8a7b40837631bdec3415d03a0f7
Sudarsan <sudmnr11@gmail.com>
2016-04-25 21:08:52 +0530

    Update subtask.py

--- subtask.py
+++ subtask.py
@@ -1,5 +1,7 @@
-# program to read series of images from the Spitzer Data Challenge, identify the brightest pixel in the images and produces a lightcurve (flux vs.time) for that pixel.
-# The mean of the maximum flux per frame for 64 frames is taken
+# Data used : XO-3_b_sim_ch2_150702 
+# Simulated Data is used for this program. 
+# Link to data : http://conference.ipac.caltech.edu/iracaas224/data_challenge/data/XO-3_b_sim_ch2_150702.tgz
+
 import pyfits 
 import numpy as np
 import glob
@@ -13,62 +15,121 @@ import dateutil.parser
 import matplotlib.pyplot as plt
 from matplotlib import pyplot
 from datetime import datetime
+import operator
+from collections import defaultdict
 
-mtable=[]	#This list holds the values for making the CSV table
+#This list holds the values for making the CSV table
+mtable=[]
 
+# findbrightestpixel() takes the image_data and for each frame, finds the brightest pixel. The corresponding (x,y) of the brightest pixel is stored in a dictionary and its count is incremented. After processing the 64 frames, the coordinates (x,y) which has been the brightest in maximum number of frames is returned.
+
+def findbrightestpixel(img_data,dict):
+
+	n=0
+	while( n < 64 ):
+		# To get index which holds the brightest pixel. This propogates through NAN values	
+		pixcoord= unravel_index(image_data[n].argmax(), image_data[n].shape)  
+		xpix= pixcoord[0]																		  
+		ypix= pixcoord[1]
+		dict[(xpix,ypix)]+=1
+		# To get the value of the brightest pixel.
+		maxn=np.amax(image_data[n])
+		n=n+1
+	pixcoord= max(dict.iteritems(), key=operator.itemgetter(1))[0]
+	return pixcoord
+	
+	
+# updatetable() takes updates the mtable by creating a row with the parameters passed
 def updatetable(xpix, ypix,flux,obstime):
-	obsdatetime=dateutil.parser.parse(obstime)  #To make it as a datetime object
+	#To make it as a datetime object
+	obsdatetime=dateutil.parser.parse(obstime)
 	mtable.append([xpix,ypix,flux,obsdatetime])
-	
 	return
-	
+
+# makecsv() takes a numpy array and creates a CSV file for the same with proper headers
 def makecsv(mtable):
-	mtable= sorted(mtable,key=lambda y: y[3])	# To sort based on timestamp
-	with open('brightpixeldata.csv', 'wb') as outcsv:   	    #configure writer to write standard csv file
-	    writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+	# To sort based on timestamp
+	mtable= sorted(mtable,key=lambda y: y[3])	
+	with open('brightpixeldata.csv', 'wb') as outcsv:
+		#configure writer to write standard csv file
+		writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
 	    writer.writerow(['PIXEL_X', 'PIXEL_Y', 'FLUX', 'TIMESTAMP'])
 	    for item in mtable:
-	        writer.writerow(item)		# Write item to outcsv
-	             
+	    	# Write item to outcsv
+			writer.writerow(item)		
+
+# readcsv() reads the CSV file and stores the data in lists
 def readcsv(csvfilename):
 	colnames = ['PIXEL_X','PIXEL_Y','FLUX','TIMESTAMP']
 	path = "/home/hema/Documents/mcgill/main/"+csvfilename
 	data = pandas.read_csv(path, names=colnames)
-											# Read the CSV file data and store in lists
+	# Read the CSV file data and store in lists
 	x = data.PIXEL_X.tolist()
 	y = data.PIXEL_Y.tolist()
 	fluxlist= data.FLUX.tolist()
 	timestmp = data.TIMESTAMP.tolist()
 	i=1
 	obsdate=[]
-	count=[]								
+	count=[]				
 	while ( i < len( timestmp) ):
 		count.append(i)
-		date_object = datetime.strptime(timestmp[i] ,'%Y-%j-%d %H:%M:%S.%f')
-		timestmp[i]=time.mktime(date_object.timetuple())		#Convert the timestamp to seconds. 
+		try:
+			date_object = datetime.strptime(timestmp[i] ,'%Y-%j-%d %H:%M:%S.%f')	
+		except ValueError:
+			date_object = datetime.strptime(timestmp[i] ,'%Y-%j-%d %H:%M:%S')
+		#Convert the timestamp to seconds.
+		timestmp[i]=time.mktime(date_object.timetuple()) 
 		obsdate.append( timestmp[i])
 		i=i+1
+	
 	print "CSV file read"
 	print "Plotting light curve"
-	#plotlightcurve(count,fluxlist[1:])
+	#Plot (flux vs. time) light curve
 	plotlightcurve(obsdate,fluxlist[1:])
 
-							
-def plotlightcurve(xaxis,yaxis):									#Plot (flux vs. time) light curve
+# plotlightcurbe() takes the axes lists for which a lightcurve will be plotted
+
+def plotlightcurve(xaxis,yaxis):									
 	pyplot.plot(xaxis,yaxis,'ro')
-	pyplot.title('Light curve for brightest pixel in the sample')
+	# X axis : Time of observation ( taken from FITS header ) in seconds
+	# Y axis : Flux in MJ/st
+	pyplot.title('Light curve for brightest pixel in the sample')   
 	pyplot.xlabel('Time of observation (s)')
 	pyplot.ylabel('Flux (MJ/st)')
 	pyplot.grid(True)
 	pyplot.show()
+	pyplot.close()
+
 	       
 # MAIN
-	
-direc=20150005			#AOR directory name for including in path initialised with the starting value of directory
-ndirec=1			# Number of directories to collect images from
+
+#AOR directory name for including in path initialised with the starting value of directory
+direc=20150000
+# Number of directories to collect images from
+ndirec=1			
 ct =0;	
+
+# Dictionary that holds the coordinates of potential brightest pixels and their count
+dict = defaultdict(int)
+
 for i in range(ndirec):
-	path = "/home/hema/Documents/mcgill/XO-3_b_sim_ch2_150702/r"+str(direc)+"/ch2/bcd"		# Resolving path to read file
+	# Resolving path to read file
+	path = "/home/hema/Documents/mcgill/XO-3_b_sim_ch2_150702/r"+str(direc)+"/ch2/bcd"		
+	for filename in glob.glob(os.path.join(path, '*bcd.fits')):
+		f=pyfits.open(filename)
+		image_data = f[0].data
+		# bpx holds coordinates of the brightest pixel
+		bpx= findbrightestpixel(image_data,dict)				
+# x coordinate of brightest pixel
+xpix=bpx[0]										
+# y coordinate of brightest pixel
+ypix=bpx[1]										
+		
+		
+		
+for i in range(ndirec):
+	# Resolving path to read file
+	path = "/home/hema/Documents/mcgill/XO-3_b_sim_ch2_150702/r"+str(direc)+"/ch2/bcd"		
 	for filename in glob.glob(os.path.join(path, '*bcd.fits')):
 		ct = ct +1
 		f=pyfits.open(filename)
@@ -77,19 +138,18 @@ for i in range(ndirec):
 		sumofmax=0.0
 		n=0
 		while( n < 64 ):
-			xpix= 15											  # Hardcoding brightest pixel value based on manual observation
-			ypix= 15
-			maxn=image_data[n][xpix][ypix]											  # Intensity is the intensity of (xpix,ypix)
+			# Intensity is the intensity of (xpix,ypix)
+			maxn=image_data[n][xpix][ypix]											  
 			sumofmax += maxn
 			n=n+1
-		avgflux = sumofmax/64							 # Finding the mean of the flux of brightest pixel for the 64 frames
+		# Finding the mean of the flux of brightest pixel for the 64 frames
+		avgflux = sumofmax/64							 
 		updatetable(xpix,ypix,avgflux,header['DATE_OBS'] )
 	direc+=1
-	print path					# Printing the path of directories scanned for images
+	# Printing the path of directories scanned for images
+	print path					
 
 print "Number of images processed:",ct
-#print "UPD:", upd
 makecsv(mtable)
 print "CSV file created"
 readcsv("brightpixeldata.csv")
-
