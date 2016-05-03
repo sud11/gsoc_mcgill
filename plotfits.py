dcec4d9e14d68e7b7e7438f5ea6524a956db6ed2
sud11 <=>
2016-05-03 18:57:54 +0530

    Included option to plot  median image of many frames

--- plotfits.py
+++ plotfits.py
@@ -1,16 +1,32 @@
 # Plot FITS images and save them in PDF
+# Plot FITS images and save them in PDF
+# Corrected to include mediam image of 64 frames
 import pylab
 import pyfits
 from matplotlib.backends.backend_pdf import PdfPages
+import pylab
+import pyfits
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 
-pdfname = 'image_pdf.pdf'
+pdfname = 'imagespdf.pdf'
 with PdfPages(pdfname) as pdf:
 	#Open the FITS image
-	f = pyfits.open('SPITZER_I2_46467072_0000_0000_2_bcd.fits')
+	f = pyfits.open('SPITZER_I2_20150000_0000_0000_1_bcd.fits')
 	image_data = f[0].data
 	tam = image_data[0].shape
+	#stacking 64 into 1 image by taking pixel wise median
+	stacked=np.median( image_data, axis=0)	
+	pylab.clf()
+	pylab.gray()
+	pylab.imshow(stacked)
+	pylab.title('Median image of 64 frames')
+	pylab.xlabel('x pixels')
+	pylab.ylabel('y pixels')
+	pylab.ylim([0, tam[0]])
+	pylab.xlim([0, tam[1]])
+	pdf.savefig()
 	#Plot image for each frame
-	# if loop is removed,one frame of image will be plotted
 	for frameno in range (1,64):
 		pylab.clf()
 		pylab.gray();
